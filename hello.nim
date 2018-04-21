880fe2418e633c6064f2aa6ee4d9704d97c63de4
鵜巷 <u@chimata.org>
2018-04-22 03:15:51 +0900

    モジュールとしてインポート可能に(hello_brainfuckでお試し

--- hello.nim
+++
@@ -1,5 +0,0 @@
-var x = 0
-for i in 1 .. 100_000_000:
-  inc x # increase x, this is a comment btw
-
-echo "Hello World ", x
--- src/brainfuck.nim
+++ src/brainfuck.nim
@@ -1,45 +1,47 @@
-# Hello Nim!
-echo "Welcome to brainfuck"
+proc interpret*(code: string) =
+  ## Interprets the brainfuck `code` string, reading from stdin and writing to
+  ## stdout.
 
-import os
+  var
+    tape: seq[char] = newSeq[char]()
+    codePos: int = 0
+    tapePos: int = 0
 
-var
-  tape: seq[char] = newSeq[char]()
-  codePos: int = 0
-  tapePos: int = 0
+  {.push overflowchecks: off.}
+  proc xinc(c: var char) = inc c
+  proc xdec(c: var char) = dec c
+  {.pop.}
 
-let code = if paramCount() > 0: readFile paramStr(1)
-           else: readAll stdin
+  proc run(skip = false): bool =
+    while tapePos >= 0 and codePos < code.len:
+      if tapePos >= tape.len:
+        tape.add '\0'
 
-echo code
+      if code[codePos] == '[':
+        inc codePos
+        let oldPos = codePos
+        while run(tape[tapePos] == '\0'):
+          codePos = oldPos
+      elif code[codePos] == ']':
+        return tape[tapePos] != '\0'
+      elif not skip:
+        case code[codePos]
+        of '+': xinc tape[tapePos]
+        of '-': xdec tape[tapePos]
+        of '>': inc tapePos
+        of '<': dec tapePos
+        of '.': stdout.write tape[tapePos]
+        of ',': tape[tapePos] = stdin.readChar
+        else: discard
 
-{.push overflowchecks: off.}
-proc xinc(c: var char) = inc c
-proc xdec(c: var char) = dec c
-{.pop.}
-
-proc run(skip = false): bool =
-  while tapePos >= 0 and codePos < code.len:
-    if tapePos >= tape.len:
-      tape.add '\0'
-
-    if code[codePos] == '[':
       inc codePos
-      let oldPos = codePos
-      while run(tape[tapePos] == '\0'):
-        codePos = oldPos
-    elif code[codePos] == ']':
-      return tape[tapePos] != '\0'
-    elif not skip:
-      case code[codePos]
-      of '+': xinc tape[tapePos]
-      of '-': xdec tape[tapePos]
-      of '>': inc tapePos
-      of '<': dec tapePos
-      of '.': stdout.write tape[tapePos]
-      of ',': tape[tapePos] = stdin.readChar
-      else: discard
 
-    inc codePos
+  discard run()
 
-discard run()
+when isMainModule:
+  import os
+
+  let code = if paramCount() > 0: readFile paramStr(1)
+             else: readAll stdin
+
+  interpret code
